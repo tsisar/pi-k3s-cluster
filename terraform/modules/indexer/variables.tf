a6d7a7af7078cceb58ca35ff78edec4d701e88ad
Tsisar <fhantom.tp@gmail.com>
2025-06-12 15:10:18 +0400

    Added Indexer Module for local development

---
+++ terraform/modules/indexer/variables.tf
@@ -0,0 +1,44 @@
+variable "name" {
+  description = "The name of the project"
+  type        = string
+}
+
+variable "namespace" {
+  description = "Namespace"
+  type        = string
+}
+
+variable "type" {
+  description = "Type"
+  type        = string
+  default     = "indexer"
+}
+
+variable "project" {
+  description = "Project"
+  type        = string
+  default     = "indexer"
+}
+
+variable "network" {
+  description = "Network"
+  type        = string
+  default     = "crosschain"
+}
+
+variable "branch" {
+  description = "Branch to deploy"
+  type        = string
+  default     = "dev"
+}
+
+variable "host" {
+  description = "The external host"
+  type        = string
+}
+
+variable "hasura_user" {
+  description = "Hasura user"
+  type        = string
+  default     = "admin"
+}
